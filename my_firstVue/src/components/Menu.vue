77db4efd1b3d2700c77e33acefddb5e679950b9b
qinhaochen <2830150476@qq.com>
2022-12-09 10:47:51 +0800

    first

---
+++ my_firstVue/src/components/Menu.vue
@@ -0,0 +1,10 @@
+<template>
+  <el-menu>
+    <el-menu-item index="1">item one</el-menu-item>
+    <el-menu-item index="2">item one</el-menu-item>
+    <el-menu-item>item one</el-menu-item>
+    <el-menu-item>item one</el-menu-item>
+    <el-menu-item>item one</el-menu-item>
+    <el-menu-item>item one</el-menu-item>
+  </el-menu>
+</template>
---
+++ my_firstVue/src/views/home/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px" float-left>
+        <Menu/>
+      </el-aside>
+      <el-main>
+        <div class="float-right">a</div>
+        Main
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import Menu from '../../components/Menu.vue'
+
+
+
+onMounted(()=>{
+  console.log("aaaa");
+})
+</script>
+
+<style scoped>
+
+</style>
